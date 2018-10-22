1177bd92c017725f98c78e03abc7beb935759de4
linzero <2574025870@qq.com>
2018-10-23 00:31:16 +0800

    add

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -16,6 +16,9 @@ Page({
         url:'http://bl.7yue.pro/v1/classic/latest',
         header:{
           appkey:"KOLDaSADHDLWWbF"
+        },
+        success:function(res){
+              console.log(res)
         }
       })
   },
